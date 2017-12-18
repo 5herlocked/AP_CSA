33df8c1f25425df9aac4afc43bc71a63d67bf93a
Shadow <31039336+5herlocked@users.noreply.github.com>
2017-12-18 09:58:21 -0500

    Playing about

--- Chapter 6 Labs/OneNoteLabs/PitchCount.java
+++ Chapter 6 Labs/OneNoteLabs/PitchCount.java
@@ -6,13 +6,7 @@ import console.*;
 import java.text.*;
 
 public class PitchCount {
-	public static int[][] pitchArray = {
-			{45, 105, 67},//pitchArray[0].length
-			{81, 100, 93, 25, 128, 88},
-			{45, 53},
-			{79, 107, 53, 79},
-			{11, 9, 3, 63, 20, 18, 3},
-		};
+	/*
 	private static int getLargest (int pitcherNumber) {
 		int temp = Integer.MIN_VALUE;
 		for (int i : pitchArray[pitcherNumber - 1]) {
@@ -37,13 +31,37 @@ public class PitchCount {
 	private static double getAverage (int pitcherNumber) {
 		return (double)getSum(pitcherNumber)/pitchArray[pitcherNumber - 1].length;
 	}
+	*/
+
 	public static void main (String... args){
+		int[][] pitchArray = {
+			{45, 105, 67},//pitchArray[0].length
+			{81, 100, 93, 25, 128, 88},
+			{45, 53},
+			{79, 107, 53, 79},
+			{11, 9, 3, 63, 20, 18, 3},
+		};
+		String another;
+		do{
+			int pitcherInfoNum = Console.promptInt("What pitchers info do you want?");
+			int averageCount, largest = Integer.MIN_VALUE, smallest = Integer.MAX_VALUE, total, sum = 0;
 
-		int pitcherInfoNum = Console.promptInt("What pitchers info do you want?");
+			for (int i = 0; i < 1; i ++) {
+				for (int j : pitchArray[pitcherInfoNum - 1])
+					largest = (j > largest) ? j : largest;
+				
+				for (int k : pitchArray[pitcherInfoNum - 1])
+					smallest = (k < smallest) ? k : smallest;
+				
+				for (int l : pitchArray[pitcherInfoNum - 1])
+					sum += l;
+			}
 
-		System.out.println (MessageFormat.format("Pitcher #{0} pitched {1} games." + 
-			" His largest pitch count was; {2}, smallest was: {3}, total was: {4}," + 
-			" and the average pitch count was: {5}", pitcherInfoNum, pitchArray[pitcherInfoNum-1].length, getLargest(pitcherInfoNum),
-			getSmallest(pitcherInfoNum), getSum(pitcherInfoNum), getAverage(pitcherInfoNum)));
+			System.out.println (MessageFormat.format("Pitcher #{0} pitched {1} games." + 
+				" His largest pitch count was; {2}, smallest was: {3}, total was: {4}," + 
+				" and the average pitch count was: {5}", pitcherInfoNum, pitchArray[pitcherInfoNum-1].length, largest,
+				smallest, sum, (double)sum/pitchArray[pitcherInfoNum-1].length));
+			another = Console.promptString("Do you want the information of another pitcher?(y/n)");
+		}while (another.equalsIgnoreCase("y"));
 	}
 }
