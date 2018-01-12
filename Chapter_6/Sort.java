570b0878a1bd09c9dacac31344ccf4f2d1fcb612
Shadow <31039336+5herlocked@users.noreply.github.com>
2018-01-12 13:50:38 -0500

    Sortin' shit

--- Chapter_6/Sort.java
+++ Chapter_6/Sort.java
@@ -4,8 +4,30 @@ import console.*;
 public class Sort {
 	public static void main (String ... args) {
 		int[] nums = {16, 51, 68, 13, 3, 81};
+		
+		///*
+		int min, minIndex;//Selection Sort
 
-		for (int i = 0; i < nums.length; i++){
+		for (int i = 0; i < nums.length-1; i++)
+		{
+			min = nums[i];
+			minIndex = i;
+			for (int j = i + 1; j < nums.length; j++)
+			{
+				if (nums[j] < min)
+				{
+					min = nums[j];
+					minIndex = j;
+				}
+			}
+			int temp = nums[i];
+			nums[i] = nums[minIndex];
+			nums[minIndex] = temp;
+			System.out.println (Arrays.toString(nums));
+		}
+		//*/
+		/*
+		for (int i = 0; i < nums.length; i++){ //my sort
 			int temp;
 			//sort ascending or descending (just change the if statement)
 			for (int c = 0; c < nums.length; c++) {
@@ -15,7 +37,8 @@ public class Sort {
 					nums[c] = temp;
 				}
 			}
+			System.out.println (Arrays.toString(nums));
 		}
-		System.out.println (Arrays.toString(nums));
+		*/
 	}
 }
